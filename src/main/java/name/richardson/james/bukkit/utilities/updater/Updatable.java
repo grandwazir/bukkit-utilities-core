b1274b675d0a461f50e8245efda7e09ad4ef956d
James Richardson <me+github@james.richardson.name>
2012-06-10 21:00:07 +0100

    Add Updatable interface for plugins to use.

---
+++ src/main/java/name/richardson/james/bukkit/utilities/updater/Updatable.java
@@ -0,0 +1,55 @@
+package name.richardson.james.bukkit.utilities.updater;
+
+import java.net.URL;
+import java.util.List;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface Updatable.
+ */
+public interface Updatable {
+
+  /**
+   * Update the plugin if required. 
+   *
+   * @param version the version to update to. If null will attempt to update to the latest version.
+   * @return true, if successful
+   */
+  public boolean update(String version);
+  
+  /**
+   * Gets a list containing all the versions managed by Maven ordered by release date in ascending order.
+   *
+   * @return the version list
+   */
+  public List<String> getVersionList();
+  
+  /**
+   * Checks if is new version available.
+   *
+   * @return true, if is new version available
+   */
+  public boolean isNewVersionAvailable();
+  
+  /**
+   * Gets the group id of the Maven artifact.
+   *
+   * @return the group id
+   */
+  public String getGroupID();
+  
+  /**
+   * Gets the artifact id of the Maven project.
+   *
+   * @return the artifact id
+   */
+  public String getArtifactID();
+  
+  /**
+   * Gets the url of the repository that contains this plugin.
+   *
+   * @return the repository url
+   */
+  public URL getRepositoryURL();
+  
+}
