73bbedb9d7e4496f0bae68e29d519e98d7e7f03d
James Richardson <me+github@james.richardson.name>
2012-07-07 01:20:09 +0100

    add helper methods

--- src/main/java/name/richardson/james/bukkit/utilities/persistence/SQLStorage.java
+++ src/main/java/name/richardson/james/bukkit/utilities/persistence/SQLStorage.java
@@ -38,36 +38,35 @@ public abstract class SQLStorage implements Storage {
   }
   
   public int count(final Class<?> recordClass) {
+    this.logger.debug("Attempting to get row count for eBean " + recordClass.getSimpleName() + ".");
     if (!classes.contains(recordClass)) throw new IllegalArgumentException();
-    this.logger.debug("Attempting to get row count matching a specific class.");
     return this.database.find(recordClass).findRowCount();
   }
 
   public int count(final Object record) {
     if (!classes.contains(record)) throw new IllegalArgumentException();
-    this.logger.debug("Attempting to get row count using an example.");
+    this.logger.debug("Attempting to get row count for " + record.getClass().getSimpleName() + " using an example eBean.");
     this.logger.debug(record.toString());
     final ExampleExpression expression = this.database.getExpressionFactory().exampleLike(record, true, LikeType.EQUAL_TO);
     return this.database.find(record.getClass()).where().add(expression).findRowCount();
   }
 
   public int delete(final List<? extends Object> records) {
+    this.logger.debug("Deleting " + String.valueOf(records.size()) + " record(s) from database.");
     if (records.isEmpty() || !classes.contains(records.get(0).getClass())) throw new IllegalArgumentException();
-    this.logger.debug("Deleting records from database.");
-    this.logger.debug(this.toString());
     return this.database.delete(records);
   }
 
   public void delete(final Object record) {
+    this.logger.debug("Deleting " + record.getClass().getSimpleName() + " from database.");
     if (!classes.contains(record)) throw new IllegalArgumentException();
-    this.logger.debug("Deleting record from database.");
     this.logger.debug(this.toString());
     this.database.delete(record);
   }
 
   public List<? extends Object> find(final Object record) {
     if (!classes.contains(record)) throw new IllegalArgumentException();
-    this.logger.debug("Attempting to return records matching an example.");
+    this.logger.debug("Attempting to return records " + record.getClass().getSimpleName() + " using an example eBean.");
     this.logger.debug(record.toString());
     final ExampleExpression expression = this.database.getExpressionFactory().exampleLike(record, true, LikeType.EQUAL_TO);
     return this.database.find(record.getClass()).where().add(expression).findList();
@@ -78,26 +77,19 @@ public abstract class SQLStorage implements Storage {
   }
 
   public List<? extends Object> list(final Class<?> record) {
+    this.logger.debug("Attempting to return all records matching the class: " + record.getName());
     if (!classes.contains(record)) throw new IllegalArgumentException();
-    this.logger.debug("Attempting to return records matching the class: " + record.getName());
     this.logger.debug(record.toString());
     return this.database.find(record).findList();
   }
 
-  public List<? extends Object> list(final Object record) {
-    this.logger.debug("Attempting to return records matching an example.");
-    this.logger.debug(record.toString());
-    final ExampleExpression expression = this.database.getExpressionFactory().exampleLike(record, true, LikeType.EQUAL_TO);
-    return this.database.find(record.getClass()).where().add(expression).findList();
-  }
-
   public int save(final List<? extends Object> records) {
-    this.logger.debug("Saving records to database.");
+    this.logger.debug("Saving " + String.valueOf(records.size()) + " record(s) to the database.");
     return this.database.save(records);
   }
 
   public void save(final Object record) {
-    this.logger.debug("Saving record to database.");
+    this.logger.debug("Saving " + record.getClass().getSimpleName() + " to the database.");
     this.logger.debug(record.toString());
     this.database.save(record);
   }
