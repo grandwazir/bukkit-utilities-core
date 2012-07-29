7901eb05f054f6d4b1519c3151e77beb884cd1b5
James Richardson <me+github@james.richardson.name>
2012-07-29 19:04:21 +0100

    copyright

--- src/main/java/name/richardson/james/bukkit/utilities/formatters/TimeFormatter.java
+++ src/main/java/name/richardson/james/bukkit/utilities/formatters/TimeFormatter.java
@@ -26,7 +26,7 @@ public class TimeFormatter {
   public final static long HOURS = 24;
   public final static long MINUTES = 60;
   public final static long SECONDS = 60;
-  
+
   public final static long ONE_SECOND = 1000;
   public final static long ONE_MINUTE = ONE_SECOND * 60;
   public final static long ONE_HOUR = ONE_MINUTE * 60;
