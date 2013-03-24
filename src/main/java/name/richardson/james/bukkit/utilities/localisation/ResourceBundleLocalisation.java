d4b056127ccc6ccad1e46891eb15b74812339ea0
James Richardson <me+github@james.richardson.name>
2013-03-24 14:11:02 +0000

    refreshed resource bundle localisation

--- src/main/java/name/richardson/james/bukkit/utilities/localisation/ResourceBundleLocalisation.java
+++ src/main/java/name/richardson/james/bukkit/utilities/localisation/ResourceBundleLocalisation.java
@@ -19,47 +19,111 @@
 package name.richardson.james.bukkit.utilities.localisation;
 
 import java.text.MessageFormat;
+import java.util.Locale;
 import java.util.ResourceBundle;
 
 import name.richardson.james.bukkit.utilities.formatters.ColourFormatter;
 
-public class ResourceBundleLocalisation extends AbstractLocalisation {
+// TODO: Auto-generated Javadoc
+/**
+ * The Class ResourceBundleLocalisation.
+ */
+public class ResourceBundleLocalisation implements Localizable {
 
+  /** The resource bundles that form this localisation. */
   private final ResourceBundle[] bundles;
 
+  /**
+   * Instantiates a new resource bundle localisation package.
+   *
+   * @param bundles the bundles
+   */
   public ResourceBundleLocalisation(final ResourceBundle... bundles) {
     super();
     this.bundles = bundles;
   }
 
-  public String getMessage(final Object object, final String key) {
-    return ColourFormatter.replace(this.getRawMessage(object, key));
+  /**
+   * Gets the locale.
+   *
+   * @return the locale
+   */
+  public Locale getLocale() {
+    return Locale.getDefault();
   }
 
-  public String getMessage(final Object object, final String key, final Object... elements) {
-    final MessageFormat formatter = new MessageFormat(this.getMessage(object, key));
-    formatter.setLocale(this.getLocale());
-    final String message = formatter.format(elements);
-    return ColourFormatter.replace(message);
+  /* (non-Javadoc)
+   * @see name.richardson.james.bukkit.utilities.localisation.Localisation#getMessage(java.lang.Object, java.lang.String)
+   */
+  public String getMessage(final Object object, final String subkey) {
+    String key = this.getPrefix(object) + "." + subkey;
+    String message = ColourFormatter.replace(this.getRawMessage(key));
+    return message;
   }
 
-  private String getClassName(final Object object) {
+  /* (non-Javadoc)
+   * @see name.richardson.james.bukkit.utilities.localisation.Localisation#getMessage(java.lang.Object, java.lang.String, java.lang.Object[])
+   */
+  public String getMessage(final Object object, final String subkey, final Object... elements) {
+    String key = this.getPrefix(object) + "." + subkey;
+    MessageFormat formatter = new MessageFormat(this.getRawMessage(key));
+    formatter.setLocale(this.getLocale());  
+    String message = formatter.format(elements);
+    message = ColourFormatter.replace(message);
+    return message;
+  }
+  
+  /* (non-Javadoc)
+   * @see name.richardson.james.bukkit.utilities.localisation.Localizable#getMessage(java.lang.String)
+   */
+  public String getMessage(String key) {
+    String message = this.getRawMessage(key);
+    return message;
+  }
+  
+  /* (non-Javadoc)
+   * @see name.richardson.james.bukkit.utilities.localisation.Localizable#getMessage(java.lang.String, java.lang.Object[])
+   */
+  public String getMessage(String key, Object... elements) {
+    MessageFormat formatter = new MessageFormat(this.getRawMessage(key));
+    formatter.setLocale(this.getLocale());  
+    String message = formatter.format(elements);
+    message = ColourFormatter.replace(message);
+    return message;
+  }
+
+  /**
+   * Gets the prefix for the message.
+   * 
+   * Unless a string is provided it will determine the class of the object provided and return the SimpleName as a prefix.
+   *
+   * @param object the object
+   * @return prefix
+   */
+  private String getPrefix(final Object object) {
     if (object instanceof Class) {
       final Class<?> c = (Class<?>) object;
       return c.getSimpleName().toLowerCase();
+    } else if (object instanceof String) {
+      return object.toString();
     } else {
       return object.getClass().getSimpleName().toLowerCase();
     }
   }
 
-  private String getRawMessage(final Object object, String key) {
-    key = this.getClassName(object) + "." + key;
+  /**
+   * Get a raw message from a localisation bundle.
+   *
+   * @param key the key
+   * @return message
+   */
+  private String getRawMessage(String key) {
     for (final ResourceBundle bundle : this.bundles) {
       if (bundle.containsKey(key)) {
         return bundle.getString(key);
       }
     }
-    return key.toUpperCase();
+    return key.toUpperCase() + " NOT TRANSLATED!";
   }
 
 }
