3615ce7fa0a3da02402b478f09f2b54d333b6c0d
James Richardson <me+github@james.richardson.name>
2014-02-09 15:22:27 +0000

    added invalid database configuration value test

---
+++ src/test/java/name/richardson/james/bukkit/utilities/persistence/configuration/InvalidDatabaseConfigurationTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2014 James Richardson.
+ *
+ * InvalidDatabaseConfigurationTest.java is part of BukkitUtilities.
+ *
+ * bukkit-utilities is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ *
+ * bukkit-utilities is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * bukkit-utilities. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package name.richardson.james.bukkit.utilities.persistence.configuration;
+
+import java.io.IOException;
+
+import com.avaje.ebean.config.ServerConfig;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class InvalidDatabaseConfigurationTest extends AbstractDatabaseConfigurationTest {
+
+	private static final String TEMPLATE_YML = "database-invalid.yml";
+
+	@Rule
+	public TemporaryFolder folder = new TemporaryFolder();
+	private DatabaseConfiguration configuration;
+	private ServerConfig serverConfig;
+
+	@Test
+	public void hasIsolationLevelBeenSetToDefaultValue() {
+		assertTrue(configuration.getDataSourceConfig().getIsolationLevel() == serverConfig.getDataSourceConfig().getIsolationLevel());
+	}
+
+	@Before
+	public void setUp()
+	throws Exception {
+		serverConfig = getDefaultServerConfig();
+		configuration = new SimpleDatabaseConfiguration(folder.newFile("database.yml"), getClass().getClassLoader().getResourceAsStream(TEMPLATE_YML), getDefaultServerConfig(), "Test");
+	}
+
+}
